81f9a8254c72956c449e529c279fbf169a1b8a9d
ras1k <rasikabdullah@gmail.com>
2022-02-17 04:54:18 +0600

    added-js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-document
+document.getElementById('calculate-button').addEventListener('click', function(){
+    const incomeInput = document.getElementById('income-input');
+    const newIncomeText = incomeInput.value;
+    const newIncome = parseInt (newIncomeText);
+    console.log(newIncome);
+});
